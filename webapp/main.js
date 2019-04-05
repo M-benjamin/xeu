522a91cc466532e21a9a7cb4e0b8f3e5d18f83ec
benjaminmballa <mballabenjamin@yahoo.fr>
2019-04-05 09:25:35 +0200

    Pwa front

--- webapp/main.js
+++ webapp/main.js
@@ -1,64 +1,19 @@
-// > RESOURCES
-let mangaHistory = [];
-const API_BASE = 'https://api.jikan.moe/v3';
+// // // > RESOURCES
+// const axios = require('axios');
+const API_KEY = '1ebc87f464422af0b44036ffb35a4c24';
+const API_BASE = 'http://data.fixer.io/api/latest?access_key=';
 const STORAGE_HISTORY_KEY = "pwanime.history"
-const API_MANGA = `${API_BASE}/search/manga?limit=12&genre=1`;
+const API_FIXER = `${API_BASE}${API_KEY}&base=EUR`;
 
-function addMangaMarkup({ image_url, title }) {
-    return `
-        <div class="column is-1"> 
-            <div class="card is-shady">
-                <div class="card-image"> 
-                    <figure class="image">
-                        <img src="${image_url}">
-                    </figure>
-                </div>
-                <div class="card-content">
-                    <div class="content">
-                        <h4>${title}</h4> 
-                    </div>
-                </div>
-            </div>               
-        </div>
-    `
-}
+async function getData() {
+    const response = await axios.get(API_FIXER)
+    // const { results } = await response
+    console.log('RES', response)
+    return response.data
+} 
 
-function updateHistory(arrayManga) {
-    mangaHistory = mangaHistory.concat(arrayManga)
-    addMangaToMarkupSelector(arrayManga, '#history')
-    localStorage.setItem(STORAGE_HISTORY_KEY, JSON.stringify(mangaHistory))
+// getData()
 
-}
-
-function addMangaToMarkupSelector(arrayManga, selector) {
-    let el = document.querySelector(selector)
-
-    arrayManga.forEach(manga => {
-        el.innerHTML = addMangaMarkup(manga) + el.innerHTML
-    });
-}
-
-async function searchManga() {
-    const query = document.querySelector('#search').value
-
-    try {
-        const response = await fetch(`${API_MANGA}&q=${query}`)
-
-        if (!response.ok) {
-            return;
-        }
-
-        const { results } = await response.json()
-
-        // > Reset search
-        document.querySelector('#current').innerHTML = ''
-        addMangaToMarkupSelector(results, '#current')
-        updateHistory(results)
-
-    } catch (error) {
-        console.log('ERROR', error)
-    }
-}
 async function installServiceWorkerAsync() {
     let storage = JSON.parse(localStorage.getItem(STORAGE_HISTORY_KEY));
     if (storage) {
@@ -77,14 +32,14 @@ async function installServiceWorkerAsync() {
     }
 }
 
+
 document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelector("#search").addEventListener("keyup", (event) => {
         if (event.keyCode === 13) {
             event.preventDefault();
-            document.querySelector("#button-search").click();
+            // document.querySelector("#button-search").click();
         }
     });
 
     installServiceWorkerAsync()
 });
-
